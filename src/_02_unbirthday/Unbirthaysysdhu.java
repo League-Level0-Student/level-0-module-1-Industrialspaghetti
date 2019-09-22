8611acae913fcdb9c453fe69f718eeece5665574
League Student <league@iMac-24.attlocal.net>
2019-09-22 14:30:47 -0700

    happy unbirthday

---
+++ src/_02_unbirthday/Unbirthaysysdhu.java
@@ -0,0 +1,17 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthaysysdhu {
+public static void main(String[] args) {
+ String birthday = JOptionPane.showInputDialog("what is your birthday?");   
+ if ( birthday.equals("September 22" )) {
+	 JOptionPane.showMessageDialog(null,"happy brithday!!!!!!!!!!");
+	 
+ }else { JOptionPane.showMessageDialog(null, "HAPPY UNBIRTHDAY");
+ }
+ }
+ } 
+
+
+                             
