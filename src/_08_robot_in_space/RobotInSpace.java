4235342d5894592bd789eba207fa5f2ef158ac88
League Student <league@iMac-34.attlocal.net>
2019-11-03 14:27:56 -0800

    did robot in space

--- src/_08_robot_in_space/RobotInSpace.java
+++ src/_08_robot_in_space/RobotInSpace.java
@@ -25,18 +25,32 @@ public class RobotInSpace implements KeyEventDispatcher {
 	 * not add code here - go to step 2
 	 */
 
+	
+
 	private void moveRobot(int keyPressed) throws InterruptedException {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
 		System.out.println(keyPressed);
 		// 3. If the up arrow is pressed, move the Robot up the screen.
-
+if (keyPressed == 38) {
+rob.setAngle(360);
+	rob.microMove(1);
+}
 		// 4. If the down arrow is pressed, move the Robot down.
-
+if (keyPressed == 39) {
+	rob.setAngle(90);
+rob.microMove(1);
+}
 		// 5. If the left arrow is pressed, make the Robot go left.
-
+if (keyPressed == 40) {
+	rob.setAngle(180);
+	rob.microMove(1);
+}
 		// 6. If right is pressed, move the Robot right.
-
+if (keyPressed == 37 ) {
+	rob.setAngle(270);
+	rob.microMove(1);
+}
 		// 7. Run your program and move the Robot to RD-2D for a surprise!
 	}
 
