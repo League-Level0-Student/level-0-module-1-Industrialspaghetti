8dc52d5dd9ea88e64ee909599d50e02b76f4bd62
League Student <league@iMac-11.attlocal.net>
2019-11-10 14:31:28 -0800

    badger thing

--- src/_11_my_ages/Myage.java
+++ src/_11_my_ages/Myage.java
@@ -2,8 +2,8 @@ package _11_my_ages;
 
 public class Myage {
 public static void main(String[] args) {
-	for(int i=0; 1<14;i++) {
-		
+	for(int i=0; i<15;i++) {
+		System.out.println(i);
 	}
 }
 }
---
+++ src/_12_badgers/ThebadgerSONG.java
@@ -0,0 +1,26 @@
+package _12_badgers;
+
+public class ThebadgerSONG {
+	public static void main(String[] args) {
+		for (int i = 0; i <3; i++) {
+			
+
+			for (int j = 0; j <12; j++) {
+				System.out.println("badger");
+				//11 2 11 2 11 2 11 5  
+			}
+						
+						for (int j = 0; j < 2; j++) {
+							System.out.println("mushroom");
+			}
+						
+		}
+	       
+		
+	for(int i = 0;i<5;i++)
+	{
+		System.out.println("Snake");
+	}
+}
+
+}
